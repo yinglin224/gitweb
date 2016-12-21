3567ca4503f3f397bfe04d663770361275736cbb
yinglin224 <songjin224@126.com>
2016-12-21 10:05:25 +0800

    gitweb测试提交

---
+++ src/com/sinosoft/GitTest.java
@@ -0,0 +1,17 @@
+package com.sinosoft;
+
+/**
+ * @Title:
+ * @Description:
+ * @Author:sjin
+ * @Since:2016-12-21 上午10:04:22
+ * @Version:1.0.0
+ * @Copyright (c) 中科软科技股份有限公司-版权所有
+ */
+
+public class GitTest {
+	public static void main(String[] args) {
+		String gitTest = "Git start test";
+		System.out.println(gitTest);
+	}
+}
